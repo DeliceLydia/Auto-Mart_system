3e63fe836466c4df3ce0b474869eeda9c60c3207
Delicelydia <Delydia84@gmail.com>
2019-06-04 21:41:20 +0200

    add signup Api [Finishes #166444372]

---
+++ server/helpers/signup.js
@@ -0,0 +1,15 @@
+import Joi from 'joi';
+
+const signup = {
+    validation(newUser) {
+        const user = {
+            first_name: Joi.string().required(),
+            last_name: Joi.string().required(),
+            email: Joi.string().email().required(),
+            address: Joi.string(),
+            is_admin: Joi.boolean()
+        };
+        return Joi.validate(newUser, user);
+    }
+};
+export default signup;
