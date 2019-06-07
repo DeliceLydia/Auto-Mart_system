ddec67fd49542b33aaa03264605bf4ee0e2a6aa7
Delicelydia <Delydia84@gmail.com>
2019-06-07 16:12:20 +0200

    add update the price of a purchase order page [Finishes #166467895]

---
+++ server/helpers/updateOrder.js
@@ -0,0 +1,11 @@
+import Joi from 'joi';
+
+const validateOrder = {
+    validation(newOrder) {
+        const userOrder = {
+            new_price_offered: Joi.number().required()
+        };
+        return Joi.validate(newOrder, userOrder);
+    }
+};
+export default validateOrder;
--- server/routes/cars.js
+++ server/routes/cars.js
@@ -1,6 +1,7 @@
 import express from'express';
 import postAD from '../controllers/postAD';
 import order from '../controllers/order';
+import updateOrder from '../controllers/updateOrder';
 import auth from '../middleware/auth';
 
 const router = express.Router();
@@ -10,5 +11,8 @@ router.post('/api/v1/car',auth, postAD);
 
 //PurchaseOrder
 router.post('/api/v1/order',auth, order );
+
+//UpdateOrderPrice
+router.patch('/api/v1/order/:id/price',auth, updateOrder);
 
 export default router;
