934def53811fd601fc4822a23c15ca664e6cb66d
Delicelydia <Delydia84@gmail.com>
2019-06-10 15:32:37 +0200

    added all changes for feedback

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 import moment from 'moment';
 import order from '../models/order';
 import users from '../models/users';
@@ -5,55 +6,55 @@ import cars from '../models/cars';
 import validateOrder from '../helpers/order';
 
 
-const Order= (req, res) => {
-    const { error } = validateOrder.validation(req.body);
-    if(error){
-        res.status(400).json({
-            status: 400,
-            error: error.details[0].message
-            
-        });
-        return;
-        
-    }
-    
-    const id = parseInt(order.length + 1,10);
-    const newOrder = {
-        id,
-        car_id: req.body.car_id,
-        amount: req.body.amount,
-        status: req.body.status || 'unsold',
-        buyer: req.body.buyer
+const Order = (req, res) => {
+  const { error } = validateOrder.validation(req.body);
+  if (error) {
+    res.status(400).json({
+      status: 400,
+      error: error.details[0].message,
 
-    };
-    const userId = users.find(c => c.id === parseInt(newOrder.buyer, 10));
-    if (!userId) {
-      return res.status(404).json({
-        status: 404,
-        error: 'buyer identification is not found',
-      });
-    }
-  
-    const carId = cars.find(e => e.id === parseInt(newOrder.car_id, 10));
-    if (!carId) {
-      return res.status(404).json({
-        status: 404,
-        error: 'car ordered not found',
-      });
-    }
-  
-    order.push(newOrder);
-    return res.status(201).json({
-      status: 201,
-      data: {
-        id,
-        car_id: newOrder.car_id,
-        created_on: moment().format('LL'),
-        status: newOrder.status,
-        price: carId.price,
-        price_offered: newOrder.amount,
-      },
     });
+    return;
+
+  }
+
+  const id = parseInt(order.length + 1, 10);
+  const newOrder = {
+    id,
+    car_id: req.body.car_id,
+    amount: req.body.amount,
+    status: req.body.status || 'unsold',
+    buyer: req.body.buyer,
+
   };
-  
-  export default Order;
+  const userId = users.find(c => c.id === parseInt(newOrder.buyer, 10));
+  if (!userId) {
+    return res.status(404).json({
+      status: 404,
+      error: 'buyer identification is not found',
+    });
+  }
+
+  const carId = cars.find(e => e.id === parseInt(newOrder.car_id, 10));
+  if (!carId) {
+    return res.status(404).json({
+      status: 404,
+      error: 'car ordered not found',
+    });
+  }
+
+  order.push(newOrder);
+  return res.status(201).json({
+    status: 201,
+    data: {
+      id,
+      car_id: newOrder.car_id,
+      created_on: moment().format('LL'),
+      status: newOrder.status,
+      price: carId.price,
+      price_offered: newOrder.amount,
+    },
+  });
+};
+
+export default Order;
--- server/controllers/signup.js
+++ server/controllers/signup.js
@@ -1,49 +1,50 @@
+/* eslint-disable linebreak-style */
 import jwt from 'jsonwebtoken';
 import validateUser from '../helpers/signup';
 import users from '../models/users';
 
 
 const signup = (req, res) => {
-    const { error } = validateUser.validation(req.body);
-    if(error){
-        res.status(400).json({
-            status: 400,
-            error: error.details[0].message
-        });
-        return;
-    }
-
-    const user = users.find(u => u.email === req.body.email);
-    if(user) {
-        res.status(400).json({
-            status:400,
-            error: 'email taken',
-        });
-        return;
-    }
-
-    const newUser = {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        password: req.body.password,
-        adress: req.body.adress,
-        is_admin: req.body.is_admin,
-    };
-    const payload = {
-        first_name: newUser.first_name,
-        last_name: newUser.last_name,
-        email: newUser.email,
-        adress: newUser.adress,
-        is_admin: newUser.is_admin,
-    };
-    const token = jwt.sign(payload, 'secret_key', { expiresIn: '24hrs' });
-    users.push(newUser);
-    res.status(201).json({
-        token,
-        status: 201,
-        data: newUser
+  const { error } = validateUser.validation(req.body);
+  if (error) {
+    res.status(400).json({
+      status: 400,
+      error: error.details[0].message,
     });
+    return;
+  }
 
-}
-export default signup
+  const user = users.find(u => u.email === req.body.email);
+  if (user) {
+    res.status(400).json({
+      status: 400,
+      error: 'email taken',
+    });
+    return;
+  }
+
+  const newUser = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: req.body.password,
+    adress: req.body.adress,
+    is_admin: req.body.is_admin,
+  };
+  const payload = {
+    first_name: newUser.first_name,
+    last_name: newUser.last_name,
+    email: newUser.email,
+    adress: newUser.adress,
+    is_admin: newUser.is_admin,
+  };
+  const token = jwt.sign(payload, 'secret_key', { expiresIn: '24hrs' });
+  users.push(newUser);
+  res.status(201).json({
+    token,
+    status: 201,
+    data: newUser,
+  });
+
+};
+export default signup;
