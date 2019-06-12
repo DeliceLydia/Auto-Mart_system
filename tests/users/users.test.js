883c36559e7a3b5b6ff5a8a63e4489506c7774df
Delicelydia <Delydia84@gmail.com>
2019-06-12 16:07:37 +0200

    added sign up tests [chore #166634182]

--- tests/users/users.test.js
+++ tests/users/users.test.js
@@ -9,21 +9,17 @@ chai.should();
 describe('signup', () => {
     it('user should be able to signup', (done) =>{
        const user={
-
-            first_name: 'Delice',
-            last_name: 'Lydia',
-            email: 'admin@gmail.com',
-            password: '123456',
-            address : 'kigali',
-            is_admin: true
+            "first_name": "777777",
+            "last_name": "parfaite",
+            "email": "lydi@gmail.com",
+            "password": "124hfjjf",
+            "address": "remera"
         };
         chai.request(app)
         .post('/api/v1/auth/signup')
         .send(user)
         .end((err, res) => {
             res.should.have.status(201);
-            res.should.be.an('object');
-            res.body.should.have.property('status').eql(201);
             res.body.should.have.property('data');
             done();
         });
@@ -31,9 +27,9 @@ describe('signup', () => {
     it('user should not be able to signup when there is incorrect data type', (done) =>{
         const user={
 
-            first_name: 'Delice',
+            first_name: '565656',
             last_name: 'Lydia',
-            email: 'admin@gmail.com',
+            email: '@gmail',
             password: '123456',
             address : 'kigali',
             is_admin: true
@@ -43,10 +39,43 @@ describe('signup', () => {
         .send(user)
         .end((err, res) => {
             res.should.have.status(400);
-            res.should.be.an('object');
-            res.body.should.have.property('status').eql(400);
-            res.body.should.have.property('data');
             done();
         });
     })
 });
+it('user should not be able to signup when there is no email', (done) =>{
+    const user={
+
+        first_name: '565656',
+        last_name: 'Lydia',
+        email: 'hduhduiewhud',
+        password: '123456',
+        address : 'kigali',
+        is_admin: true
+    };
+    chai.request(app)
+    .post('/api/v1/auth/signup')
+    .send(user)
+    .end((err, res) => {
+        res.should.have.status(400);
+        done();
+    });
+})
+it('user should not be able to signup when there is an empty field', (done) =>{
+    const user={
+
+        first_name: '565656',
+        last_name: 'Lydia',
+        email: '',
+        password: '123456',
+        address : 'kigali',
+        is_admin: true
+    };
+    chai.request(app)
+    .post('/api/v1/auth/signup')
+    .send(user)
+    .end((err, res) => {
+        res.should.have.status(400);
+        done();
+    });
+})
