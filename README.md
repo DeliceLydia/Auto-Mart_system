e6b280029964e7e07da52c4bf0aaa28e5b66868d
Delicelydia <Delydia84@gmail.com>
2019-06-13 19:28:23 +0200

    added swagger [chore #166634182]

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Auto-Mart_system
-This is an online automobiles marker.
+This is an online shop for automobiles of diverse makes, model or body type. with AUTO MART, users will be able to sell their cars or buy from trusted dealerships or private sellers
 [![Build Status](https://travis-ci.org/DeliceLydia/Auto-Mart_system.svg?branch=develop)](https://travis-ci.org/DeliceLydia/Auto-Mart_system)
 
 [![Coverage Status](https://coveralls.io/repos/github/DeliceLydia/Auto-Mart_system/badge.svg)](https://coveralls.io/github/DeliceLydia/Auto-Mart_system)
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import user_router from './routes/user';
 import postAD from './routes/cars';
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerDocument from '../swagger.json';
 
 const app = express();
 app.use(express.urlencoded({extended:false}));
@@ -12,6 +14,7 @@ app.use(user_router);
 app.use('/', postAD);
 
 const port = process.env.PORT || 3000 ;
+app.use('/apis-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(port, () => {console.log(`app is listening on ${port}`)});
 
 export default app;
